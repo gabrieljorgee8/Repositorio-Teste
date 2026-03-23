43b2cc323a367257dd165f4f79d9df34202114a9
gabrieljorgee8 <gabriel.aj@puccampinas.edu.br>
2026-03-23 00:54:27 -0300

    Parte 2- Registro de intervalo

--- Projeto-tarefa.py
+++ Projeto-tarefa.py
@@ -1,3 +1,5 @@
 print("SISTEMA DE PRESENÇA INICIADO")
 inicio_de_turno= "8:00"
 print("Horario de entrada registrado", inicio_de_turno)
+hora_do_almoco="12:00"
+print("Horario registrado para almoço", hora_do_almoco)
