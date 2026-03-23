3293d9d9e4dcd8a69998cdba0b54f8ed4b312039
gabrieljorgee8 <gabriel.aj@puccampinas.edu.br>
2026-03-23 00:57:40 -0300

    Parte 3- Turno Finalizado

--- Projeto-tarefa.py
+++ Projeto-tarefa.py
@@ -3,3 +3,6 @@ inicio_de_turno= "8:00"
 print("Horario de entrada registrado", inicio_de_turno)
 hora_do_almoco="12:00"
 print("Horario registrado para almoço", hora_do_almoco)
+hora_saida="18:00"
+print("Horario registrado para o fim do turno," hora_saida)
+print("Turno finalizado com sucesso, até amanha e bom descanso!!")
